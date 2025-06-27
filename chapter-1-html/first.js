fd0ea0e45a2e8eb4848e45c721a12613857a3417
Adityahatake <shivanshdaksh567@gmail.com>
2025-06-27 23:19:31 +0530

    add1

---
+++ chapter-1-html/first.js
@@ -0,0 +1,13 @@
+// This is a single-line comment
+
+// Declaring a variable using let
+let name = "Alice";
+
+// Using console.log to print output
+console.log("Hello, " + name + "!");
+
+// Declaring a constant
+const greeting = "Welcome to JavaScript!";
+
+// Outputting the constant
+console.log(greeting);
