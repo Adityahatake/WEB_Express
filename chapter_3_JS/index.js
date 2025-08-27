a1823876dd1b5606c3f879df40f097f00d893a27
Adityahatake <shivanshdaksh567@gmail.com>
2025-08-27 23:45:00 +0530

    aug 27

--- chapter_3_JS/index.js
+++ chapter_3_JS/index.js
@@ -4,6 +4,8 @@ console.log(`i like peaches`);
 console.log(`i like peaches`);
 window.alert("🚀 This is a cool alert! 😎");
 
+document.getElementById("myh1").textContent="radhe radhe";
+
 //this is a comment
 /*this is 
 a multi line 
