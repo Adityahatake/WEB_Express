5b5d6df4b5814c6e04018870d8bc741d31289164
Adityahatake <shivanshdaksh567@gmail.com>
2025-10-03 22:30:26 +0530

    3 oct

---
+++ chapter_3_JS/variable_example.js
@@ -0,0 +1,7 @@
+let myName =`Aditya`;
+let age= 19;
+let status="online";
+
+document.getElementById("p1").textContent=`My  name is: ${myName}`;
+document.getElementById("p2").textContent=`My  age is: ${age}`;
+document.getElementById("p3").textContent=`My  Status is: ${status}`;
