f6ed4f0dd8bf2a3882ba0f78a00dc7be21854965
Adityahatake <shivanshdaksh567@gmail.com>
2025-10-05 23:45:55 +0530

    5 oct

--- chapter_3_JS/const.js
+++ chapter_3_JS/const.js
@@ -1,6 +1,8 @@
 const PI=3.14;
-let rad =window.prompt(`Enter Radius`);
+// let rad =window.prompt(`Enter Radius`);
 rad=document.getElementById("rad").value
-
 let circumferance=2*PI*rad;
+document.getElementById("mybutton").onclick=function(){
+    document.getElementById()
+}
 console.log(`circumference is ${circumferance}`);
