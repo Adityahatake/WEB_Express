53a39bef18328d094ddbae8b129c406d6a0e57eb
Adityahatake <shivanshdaksh567@gmail.com>
2025-10-18 23:09:24 +0530

    u

--- chapter_3_JS/random_num_genrator.js
+++ chapter_3_JS/random_num_genrator.js
@@ -9,6 +9,7 @@ console.log(`Random number (${min}-${max}): ${randomnum}`);
 
 const myButton = document.getElementById("mybutton");
 myButton.onclick = function () {
+    
     let randomnum = Math.floor(Math.random() * (max - min)) + min;
     document.getElementById("ans").textContent = randomnum;
 };
