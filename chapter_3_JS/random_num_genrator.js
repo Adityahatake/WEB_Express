00ce6ae263d9099a9539128364f142430355bb7e
Adityahatake <shivanshdaksh567@gmail.com>
2025-10-25 23:50:31 +0530

    25 oct og starts

--- chapter_3_JS/random_num_genrator.js
+++ chapter_3_JS/random_num_genrator.js
@@ -9,7 +9,6 @@ console.log(`Random number (${min}-${max}): ${randomnum}`);
 
 const myButton = document.getElementById("mybutton");
 myButton.onclick = function () {
-    
     let randomnum = Math.floor(Math.random() * (max - min)) + min;
     document.getElementById("ans").textContent = randomnum;
 };
