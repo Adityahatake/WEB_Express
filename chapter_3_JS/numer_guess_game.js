a02b4f5dc3331f2dc9d29e39e17ca2d9231eb63f
Adityahatake <shivanshdaksh567@gmail.com>
2025-11-01 16:17:28 +0530

    number game

--- chapter_3_JS/numer_guess_game.js
+++ chapter_3_JS/numer_guess_game.js
@@ -1,6 +1,20 @@
 let max=100;
-let min=1;
+let min=90;
 
-let answer=Math.floor(Math.random()*(max-min +1));
+let answer=Math.floor(Math.random()*(max-min +1))+min;
 
 console.log(answer);
+
+let running =true;
+let guess;
+let attempt=0;
+
+while(running){
+    guess=window.prompt(`Guess a number between ${min}-${max}`);
+    if(guess!=answer){
+        attempt++;
+    }else{
+        window.alert(`You guess right , the answer is ${answer} and you took ${attempt} attempts`);
+
+    }
+}
