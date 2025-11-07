c4b697ca422b19acc764770cc08a4d5a40a04947
Adityahatake <shivanshdaksh567@gmail.com>
2025-11-07 23:10:00 +0530

    add x

--- chapter_3_JS/functions.js
+++ chapter_3_JS/functions.js
@@ -4,7 +4,6 @@
 function add(a, b) {
     return a + b;
 }
-
 console.log('Function Declaration add(2,3):', add(2, 3));
 
 /* 2) Function Expression
---
+++ chapter_3_JS/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+/* 1) Function Declaration
+     - Hoisted: can be called before its definition.
+*/
+function add(a, b) {
+    return a + b;
+}
+console.log('Function Declaration add(2,3):', add(2, 3));
+
+/* 2) Function Expression
