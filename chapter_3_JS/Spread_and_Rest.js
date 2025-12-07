1e24ee1e44f77e1525408f0ecb5106c51991fa79
Adityahatake <shivanshdaksh567@gmail.com>
2025-12-07 11:40:02 +0530

    add v

--- chapter_3_JS/Spread_and_Rest.js
+++ chapter_3_JS/Spread_and_Rest.js
@@ -1,6 +1,17 @@
 let fruits=["apple", "banana" ,"banana" ,"papaya"];
 fruits.sort().reverse();
 
+// Using spread operator to copy and merge arrays
+let moreFruits = ["mango", ...fruits, "grape"];
+console.log("Merged fruits array using spread:", moreFruits);
+
+// Using rest operator in a function to accept variable number of arguments
+function printFruits(...allFruits) {
+    console.log("Fruits passed using rest:", allFruits);
+}
+printFruits("kiwi", "orange", ...fruits);
+
+// Original code to print sorted and reversed fruits
 for(let f of fruits){
     console.log(f);
 }
