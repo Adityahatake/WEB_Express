c8fe7ff096ecef410e3671cc41fc4b185dded067
Adityahatake <shivanshdaksh567@gmail.com>
2025-12-24 12:09:40 +0530

    I

---
+++ chapter_3_JS/Storing_date.js
@@ -0,0 +1,6 @@
+const date1 = new Date();
+console.log(`currnet date is: ${date1}`);
+
+//creating a custom date : 
+const customdate1= new Date(2017 ,11 , 1, 10 , 11 , 3);
+console.log(`currnet date is: ${customdate1}`);
