b7ac33e4302e6e5900affd0955e24d9d8938cb83
Adityahatake <shivanshdaksh567@gmail.com>
2025-12-26 23:23:27 +0530

    I

---
+++ chapter_3_JS/timeout.js
@@ -0,0 +1,16 @@
+// Example: Using setTimeout in JavaScript
+
+console.log("Start");
+
+// setTimeout schedules a function to run after a delay (in milliseconds)
+setTimeout(function() {
+    console.log("This message is shown after 2 seconds");
+}, 2000);
+
+console.log("End");
+
+// Output:
+// Start
+// End
+// (after 2 seconds)
+// This message is shown after 2 seconds
